a42e2b2fe5005a81b8f469a1b82e33163ba26129
osaretin <osaretin.frank10@gmail.com>
2021-08-17 23:37:08 +0100

    first commit

---
+++ printName.py
@@ -0,0 +1,10 @@
+# HNGi8 TASK 2(PRINT NAME) COMPULSORY
+#SLACK DISPLAY NAME : KodeShinobi
+#NAME: OSARIEMEN OSARETIN FRANK
+#DATE: 17TH AUGUST 2021
+
+def FullName(name):
+    print(name)
+
+name = "Osariemen Osaretin Frank"
+FullName(name)
